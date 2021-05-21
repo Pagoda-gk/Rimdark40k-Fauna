b90b41bbd9e30a3cccc69a8f72eaf8ac32f95530
Pagoda <batchelor183@gmail.com>
2021-05-21 13:50:12 +0100

    Initial commit
    Added ambulls, catachan devils, clawed fieds, cruorian war beasts, enslavers, fedridian althaks fenrisian wolves ferro-beasts, grox, khymerae, mukaali, razorwings, saeneyti, salamanders, sumpkrocs, toxic dwellers, xothic blood locusts

---
+++ Source/40kFauna/40kFauna/HeddiffGiverGoJuice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using RimWorld.Planet;
+using UnityEngine;
+using Verse;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PGD_40kFauna
+{
+    public class HediffGiver_HealthBelowThreshold : HediffGiver
+    {
+        public override void OnIntervalPassed(Pawn pawn, Hediff cause)
+        {
+            HediffSet hediffSet = pawn.health.hediffSet;
+            if (pawn.health.summaryHealth.SummaryHealthPercent <= this.threshold)
+            {
+                HealthUtility.AdjustSeverity(pawn, this.hediff, hediffSet.BleedRateTotal * 0.001f);
+                return;
+            }
+            HealthUtility.AdjustSeverity(pawn, this.hediff, -0.00033333333f);
+        }
+        float threshold = 0.5f;
+    }
+}
