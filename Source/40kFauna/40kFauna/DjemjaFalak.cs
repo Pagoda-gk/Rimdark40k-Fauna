a898a3fe0895b0fb768959b2c6ab0216d7f4aa58
Pagoda <batchelor183@gmail.com>
2021-06-06 15:05:32 +0100

    minor changes

--- Source/40kFauna/40kFauna/DjemjaFalak.cs
+++ Source/40kFauna/40kFauna/DjemjaFalak.cs
@@ -14,7 +14,7 @@ using Verse.Sound;
 namespace PGD_40kFauna
 {
 
-	[HarmonyPatch(typeof(Toil), "TryRecruit")]
+	[HarmonyPatch(typeof(Toils_Interpersonal), "TryRecruit")]
 	public static class Pawn_Toils_Interpersonal_DjemjaFalak_Recruit
 	{
 
@@ -22,20 +22,23 @@ namespace PGD_40kFauna
 		[HarmonyPostfix]
 		public static void Postfix(Toil __instance, ref Toil __result)
 		{
-								
-			Pawn pawn = __result.actor;
+			Log.Message("we're actually fixing the right thing here");
+			Pawn pawn = __instance.actor;
 			if (pawn != null)
 			{
+				Log.Message("target pawn was found");
 
 				//check for djemja falak
-	
+
 				Hediff DjemjaFalak = pawn.health.hediffSet.GetFirstHediffOfDef(PGD_40kFaunaDefOf.PGD_DjemjaFalak);
 				if (DjemjaFalak != null)
 				{
+					Log.Message("hediff was found");
 					//then drop resistance by another 5 - 20
 
 					if (pawn.guest != null && pawn.guest.resistance > 0f)
 					{
+						Log.Message("the resistance condition was met");
 						float extraResistanceDrop = Rand.Range(5, 20);
 						pawn.guest.resistance = Mathf.Max(0f, pawn.guest.resistance - extraResistanceDrop);
 						DebugActionsUtility.DustPuffFrom(pawn);
