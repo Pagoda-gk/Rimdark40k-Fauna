5f405e262231b1653702237e5f0c725696cd1895
Pagoda <batchelor183@gmail.com>
2021-06-06 02:16:57 +0100

    Small tex updates

---
+++ Source/40kFauna/40kFauna/DjemjaFalak.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using System.Reflection;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.Sound;
+
+namespace PGD_40kFauna
+{
+
+	[HarmonyPatch(typeof(Toil), "TryRecruit")]
+	public static class Pawn_Toils_Interpersonal_DjemjaFalak_Recruit
+	{
+
+
+		[HarmonyPostfix]
+		public static void Postfix(Toil __instance, ref Toil __result)
+		{
+								
+			Pawn pawn = __result.actor;
+			if (pawn != null)
+			{
+
+				//check for djemja falak
+	
+				Hediff DjemjaFalak = pawn.health.hediffSet.GetFirstHediffOfDef(PGD_40kFaunaDefOf.PGD_DjemjaFalak);
+				if (DjemjaFalak != null)
+				{
+					//then drop resistance by another 5 - 20
+
+					if (pawn.guest != null && pawn.guest.resistance > 0f)
+					{
+						float extraResistanceDrop = Rand.Range(5, 20);
+						pawn.guest.resistance = Mathf.Max(0f, pawn.guest.resistance - extraResistanceDrop);
+						DebugActionsUtility.DustPuffFrom(pawn);
+
+						//then rand.chance for brain damage - based on ticks since install
+
+						if (pawn.Dead)
+						{
+							return;
+						}
+						if (Rand.Value <= DjemjaFalak.ageTicks * 0.00005)
+						{
+							BodyPartRecord brain = pawn.health.hediffSet.GetBrain();
+							if (brain == null)
+							{
+								return;
+							}
+							int num = Rand.RangeInclusive(1, 5);
+							pawn.TakeDamage(new DamageInfo(DamageDefOf.Flame, (float)num, 0f, -1f, pawn, brain, null, DamageInfo.SourceCategory.ThingOrUnknown, null));
+						}
+					}
+
+					
+				}
+				
+			}
+
+			return;
+		}
+	}
+		
+	
+}
